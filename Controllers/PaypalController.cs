b230c15cbc295358b92fa44fdba716a6ff0131c1
Reem <reem.osama3052001@gmail.com>
2024-06-30 23:09:39 +0300

    done with email confirmation

---
+++ Controllers/PaypalController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaypalCheckoutExample.Clients;
+
+namespace PaypalCheckoutExample.Controllers
+{
+    public class PaypalController : Controller
+    {
+        private readonly PaypalClient _paypalClient;
+
+        public PaypalController(PaypalClient paypalClient)
+        {
+            this._paypalClient = paypalClient;
+        }
+
+        public IActionResult Index()
+        {
+            // ViewBag.ClientId is used to get the Paypal Checkout javascript SDK
+            ViewBag.ClientId = _paypalClient.ClientId;
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Order(CancellationToken cancellationToken)
+        {
+            try
+            {
+                // set the transaction price and currency
+                var price = "1500.00";
+                var currency = "USD";
+
+                // "reference" is the transaction key
+                var reference = "INV001";
+
+                var response = await _paypalClient.CreateOrder(price, currency, reference);
+
+                // Log the CreateOrder response
+                Console.WriteLine("Order response:");
+                Console.WriteLine(response);
+
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                var error = new
+                {
+                    e.GetBaseException().Message
+                };
+
+                return BadRequest(error);
+            }
+        }
+
+        public async Task<IActionResult> Capture(string orderId, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var response = await _paypalClient.CaptureOrder(orderId);
+
+                if (response == null)
+                {
+                    Console.WriteLine("CaptureOrder response is null");
+                    return BadRequest(new { message = "Response is null" });
+                }
+
+                //if (response.purchase_units == null || response.purchase_units.Count == 0)
+                //{
+                //    Console.WriteLine("Purchase units are null or empty");
+                //    return BadRequest(new { message = "Purchase units are null or empty" });
+                //}
+
+               
+                //var reference = response.purchase_units[0].reference_id;
+
+                // Put your logic to save the transaction here
+                // You can use the "reference" variable as a transaction key
+
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e.GetBaseException().Message}");
+                var error = new
+                {
+                    e.GetBaseException().Message
+                };
+
+                return BadRequest(error);
+            }
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
